b63659cfb127f9bf4cdb0fdc3bff107de32f94fa
Pulkit Goyal <pulkitcgoyal@gmail.com>
2025-02-01 17:56:53 +0530

    minor changes to python exp 6

--- Python Lab/exp6.py
+++ Python Lab/exp6.py
@@ -2,7 +2,8 @@
 def calculate_area_of_circle():
     import math
     radius = float(input("Enter the radius of the circle: "))
-    area = math.pi * radius ** 2
-    print(f"The area of the circle with radius {radius} units is {area} unit square.")
+    area = (math.pi * radius ** 2)
+    print(f"The area of the circle with radius {radius} units is {area:.2f} unit square.")
+    # another way to round off the area to 2 decimal places: round(area, 2)
 
 calculate_area_of_circle()
